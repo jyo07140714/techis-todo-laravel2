78f91f19235bef8b987dc4aedad7989d220c1e0e
jyo07140714 <121212816+jyo07140714@users.noreply.github.com >
2023-05-03 17:32:20 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //
         Schema::defaultStringLength(191);
+        if (\App::environment(['production']) || \App::environment(['develop'])) {
+            \URL::forceScheme('https');
+        }
     }
 }
